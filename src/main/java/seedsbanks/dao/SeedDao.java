f70c20ed57527237b77ba2f7326681bcd4754ace
Anibal Anto <anibal.anto@gmail.com>
2014-09-13 11:02:59 -0300

    creado el paquete dao

---
+++ src/main/java/seedsbanks/dao/SeedDao.java
@@ -0,0 +1,11 @@
+package main.java.seedsbanks.dao;
+
+import java.util.ArrayList;
+
+import main.java.seedsbanks.Seed;
+
+public class SeedDao {
+	public ArrayList<Seed> getSeed(){
+		return new ArrayList<Seed>();
+	}
+}
